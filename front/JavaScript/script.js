ec1f667addf6fee4f3d39018b68488210458a386
yonirg <yonirabinovicigherman@gmail.com>
2022-02-23 10:54:39 -0300

    Utilizando o DOM para buscar elementos HTML úteis para o JS

---
+++ front/JavaScript/script.js
@@ -0,0 +1,3 @@
+var DivPrincesa1 = document.getElementsByClassName('princesa')[0];
+var imgPrincesa1 = document.getElementById('princesa1');
+var descricaoPrincesa1 = document.getElementById('descricao1')

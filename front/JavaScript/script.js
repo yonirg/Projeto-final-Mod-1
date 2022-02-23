72107449653bc1371840db9176c21a1c458c1408
yonirg <yonirabinovicigherman@gmail.com>
2022-02-23 11:18:35 -0300

    Replicando mesmo comportamento para princesa 2

--- front/JavaScript/script.js
+++ front/JavaScript/script.js
@@ -14,9 +14,28 @@ function mostraPrincesa1(){
    descricaoPrincesa1.style.display='none'; 
 };
 
+var DivPrincesa2 = document.getElementsByClassName('princesa')[1];
+var imgPrincesa2 = document.getElementById('princesa2');
+var descricaoPrincesa2 = document.getElementById('descricao2')
+
+function ocultaPrincesa2() {
+    DivPrincesa2.removeChild(imgPrincesa2);
+    DivPrincesa2.style.height = '458px';      
+    DivPrincesa2.style.width = '272px';
+    descricaoPrincesa2.style.display='block';
+};
+
+function mostraPrincesa2(){
+    DivPrincesa2.appendChild(imgPrincesa2);
+   descricaoPrincesa2.style.display='none'; 
+};
+
+
 function mostrarDescricao() {  
     DivPrincesa1.addEventListener('mouseover', ocultaPrincesa1);
-    DivPrincesa1.addEventListener('mouseleave',mostraPrincesa1);   
+    DivPrincesa1.addEventListener('mouseleave',mostraPrincesa1); 
+    DivPrincesa2.addEventListener('mouseover', ocultaPrincesa2);
+    DivPrincesa2.addEventListener('mouseleave',mostraPrincesa2); 
 };
 
 mostrarDescricao();

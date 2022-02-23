57da0508fe96396431b1825a063ec2b702576d55
yonirg <yonirabinovicigherman@gmail.com>
2022-02-23 10:58:02 -0300

    Criando função que retira a imagem e mostra o parágrafo de descrição para a princesa 1

--- front/JavaScript/script.js
+++ front/JavaScript/script.js
@@ -1,3 +1,10 @@
 var DivPrincesa1 = document.getElementsByClassName('princesa')[0];
 var imgPrincesa1 = document.getElementById('princesa1');
 var descricaoPrincesa1 = document.getElementById('descricao1')
+
+function ocultaPrincesa1() {
+    DivPrincesa1.removeChild(imgPrincesa1);
+    DivPrincesa1.style.height = '458px';      
+    DivPrincesa1.style.width = '505.33px';
+    descricaoPrincesa1.style.display='block';
+};

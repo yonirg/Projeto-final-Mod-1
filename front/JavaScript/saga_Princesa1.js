8eb517a8f247ca56b2eab4faf2077899f22aeb50
yonirg <yonirabinovicigherman@gmail.com>
2022-02-25 18:29:37 -0300

    Mudança dos textos da narrativa durante o jogo

--- front/JavaScript/saga_Princesa1.js
+++ front/JavaScript/saga_Princesa1.js
@@ -1,38 +1,59 @@
-function fases (){
-    alert('A princesa arqueira, sem ser percebida, viu os soldados que estão protegendo a mansão em que o príncipe está sendo mantido.\nSelecione corretamente o que ela deve fazer agora escrevendo o número da opção desejada');
+function jogo() {
+    var narrativa1 = document.getElementById('textoNarrativa');
+    var narrativa2 = document.getElementById('textoNarrativa2');
+    var narrativa3 = document.getElementById('textoNarrativa3');
+    var narrativa4 = document.getElementById('textoNarrativa4');
+    var gameOver = document.getElementById('gameOver');
+
     var resposta1 = prompt('1 - Abater os soldados da distância que se enconta.\n2 - Se aproximar mais tentando não ser percebida para efetuar seus disparos com a flecha.\n3 - Começar a correr na direção dos soldados e ir disparando as flechas.');
-    if(resposta1 == '2'){
-        alert('Você escolheu corretamente!!!\nContinue assim nas próximas perguntas ! Vamos lá!!');
-        var resposta2 = prompt('Agora que ela neutralizou todos os soldados, o que ela deve fazer?\n 1 - Entrar na mansão correndo e procurar o príncipe para saír o mais rápido possível.\n2 - Continuar procurando outros possíveis protetores do local que eventualmente possam capturá-la.\n3 - Chamar pelo príncipe e ao mesmo tempo verificar a existência de outros soldados.');
-        if(resposta2 == '2'){            
-            var resposta3 = prompt('Ótima escolha!!!\nAgora a princesa já encontrou o príncipe, porém ele está amarrado e machucado:(\nSelecione, então, a opção para a princesa concluir a última etapa com sucesso:\n1 - Ela deve soltá-lo o mais rápido possível e eles fogem juntos, considerando que a princesa estará de prontidão com seu arco e flecha caso seja necessário.\n2 - Ela deve "vasculhar" todo o entorno da casa e dentro ddela para garantir que não haja ninguém que poderá surpreendê-los, e então deve voltar para libertar o príncipe.\n3 - Ela deve retornar para fora do castelo para tentar encontrar alguém que possa ajudá-los.');
-            if(resposta3 == '1'){
-                alert('Parabéns!!!!!!\nFinalmente o príncipe e a princesa estão a salvo e poderão viver tranquilamente!!');
-                location.assign('../html/index.html')
+    if (resposta1 == '2') {
+        narrativa1.style.display = 'none';
+        narrativa2.style.display = 'block';
 
-              
+    } else {
+        narrativa1.style.display = 'none';
+        gameOver.style.display = 'block';
+        location.assign('../html/index.html');
+
+    };
+    var resposta2;
+    var resposta3;
+    setTimeout(function () {
+        if (resposta1 == '2') {
+            resposta2 = prompt('Agora que ela neutralizou todos os soldados, o que ela deve fazer?\n 1 - Entrar na mansão correndo e procurar o príncipe para saír o mais rápido possível.\n2 - Continuar procurando outros possíveis protetores do local que eventualmente possam capturá-la.\n3 - Chamar pelo príncipe e ao mesmo tempo verificar a existência de outros soldados.');
+            if (resposta2 == '2') {
+                narrativa2.style.display = 'none';
+                narrativa3.style.display = 'block';
             } else {
-                alert('Game over! A princesa infelizmente foi capturada também');
-                location.assign('../html/index.html')
-
+                narrativa2.style.display = 'none';
+                gameOver.style.display = 'block';
+                location.assign('../html/index.html');
             };
-                
+            setTimeout(function () {
+                if (resposta2 == '2') {
+                    resposta3 = prompt('1 - Ela deve soltá-lo o mais rápido possível e eles fogem juntos, considerando que a princesa estará de prontidão com seu arco e flecha caso seja necessário.\n2 - Ela deve "vasculhar" todo o entorno da casa e dentro ddela para garantir que não haja ninguém que poderá surpreendê-los, e então deve voltar para libertar o príncipe.\n3 - Ela deve retornar para fora do castelo para tentar encontrar alguém que possa ajudá-los.');
+                } else {
 
-            } else{
-                alert('Game over! A princesa infelizmente foi capturada também');
-                location.assign('../html/index.html')
+                }
+                if (resposta3 == '1') {
+                    narrativa3.style.display = 'none';
+                    narrativa4.style.display = 'block';
+                    location.assign('../html/index.html');
 
-            };
-        } else{
-            alert('Game over! A princesa infelizmente foi capturada também');
-            location.assign('../html/index.html')
+                } else {
+                    narrativa3.style.display = 'none';
+                    gameOver.style.display = 'block';
+                    location.assign('../html/index.html');
+                };
+            }, 1000);
 
         };
+    }, 1000);
+};
 
-    }
 var botaoIniciar = document.getElementById('iniciar');
-botaoIniciar.addEventListener('click', fases);
+botaoIniciar.addEventListener('click', jogo);
 var botaoRetorno = document.getElementById('retorno');
-botaoRetorno.addEventListener('click', function(){
-location.assign('../html/index.html')
-})
+botaoRetorno.addEventListener('click', function () {
+    location.assign('../html/index.html');
+});
